87c7c88c52f374087fa28012b10f17a3da0ca81f
pengfan <pengfan@qq.com>
2025-04-25 18:04:16 +0800

    feat: 增加模块启动

---
+++ czx.go
@@ -0,0 +1,29 @@
+package czx
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var version = "1.1.11"
+
+// Module represents a module in the czx framework.
+func Run(mods ...Module) {
+	log.Printf("Czx %v starting up \n", version)
+
+	for i := range mods {
+		Register(mods[i])
+	}
+
+	Init()
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-sig
+
+	log.Printf("Czx shutting down (signal: %v)", sig)
+
+	Destroy()
+}
