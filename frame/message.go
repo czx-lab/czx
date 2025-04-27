17f7c44a47266fa02a2dc5d9854bbb9cc4d71bae
pengfan <pengfan@qq.com>
2025-04-27 16:35:06 +0800

    fix: 增加 loop消息序列化、反序列化

--- frame/message.go
+++ frame/message.go
@@ -1,6 +1,9 @@
 package frame
 
-import "time"
+import (
+	"encoding/json"
+	"time"
+)
 
 type (
 	// Message represents a single input message from a player.
@@ -17,3 +20,36 @@ type (
 		Inputs  map[string]Message
 	}
 )
+
+// Serialize serializes the Frame to JSON.
+// It uses the json package to convert the Frame struct to a JSON byte slice.
+// The function returns the serialized byte slice and any error encountered during serialization.
+func (r *Frame) Serialize() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+// Deserialize deserializes the JSON byte slice to a Frame struct.
+// It uses the json package to convert the JSON byte slice to a Frame struct.
+func (r *Frame) Deserialize(data []byte) error {
+	return json.Unmarshal(data, r)
+}
+
+// Serialize serializes the Message to JSON.
+// It uses the json package to convert the Message struct to a JSON byte slice.
+func (m *Message) Serialize() ([]byte, error) {
+	if m.Timestamp.IsZero() {
+		m.Timestamp = time.Now()
+	}
+
+	return json.Marshal(m)
+}
+
+// Deserialize deserializes the JSON byte slice to a Message struct.
+// It uses the json package to convert the JSON byte slice to a Message struct.
+func (m *Message) Deserialize(data []byte) error {
+	if m.Timestamp.IsZero() {
+		m.Timestamp = time.Now()
+	}
+
+	return json.Unmarshal(data, m)
+}
