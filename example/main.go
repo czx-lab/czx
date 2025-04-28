38c7e6fa2e7395027ddf9f58de52bb56aec022b3
pengfan <pengfan@qq.com>
2025-04-28 20:37:45 +0800

    fix: 删除客户端

--- example/main.go
+++ example/main.go
@@ -6,7 +6,17 @@ import (
 	"github.com/czx-lab/czx/agent"
 	"github.com/czx-lab/czx/eventbus"
 	"github.com/czx-lab/czx/network"
+	"github.com/czx-lab/czx/network/jsonx"
 	"github.com/czx-lab/czx/network/ws"
+)
+
+type (
+	Hello struct {
+		Name string
+	}
+	HelloReply struct {
+		Msg string
+	}
 )
 
 func main() {
@@ -20,11 +30,24 @@ func main() {
 		},
 	}
 
+	processor := jsonx.NewProcessor(network.ProcessorConf{
+		LittleEndian: false,
+	})
+
+	processor.Register(1, &Hello{})
+	processor.Register(2, &HelloReply{})
+	processor.RegisterHandler(&HelloReply{}, func(args []any) {
+		param := args[1].(*Hello)
+		args[0].(network.Agent).WriteWithCode(2, &HelloReply{
+			Msg: fmt.Sprintf("Hello %s", param.Name),
+		})
+	})
+
 	closeFlag := make(chan struct{})
 	gateway := agent.NewGate(*gateConf).WithPreConn(func(a network.Agent, phm network.ClientAddrMessage) {
 		// Pre-handler message logic here
 		fmt.Println("Pre-handler message:", phm)
-	}).WithFlag(closeFlag)
+	}).WithFlag(closeFlag).WithProcessor(processor)
 
 	go func() {
 		eventbus.DefaultBus.Subscribe(eventbus.EvtNewAgent, func(message any) {
